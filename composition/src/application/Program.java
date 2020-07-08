c37715c4eed7a25e1467b8602f71979f5bcd0835
Yves César <yvesamorim73@gmail.com>
2020-07-08 17:52:32 -0400

    Composition(Part 1)

---
+++ composition/src/application/Program.java
@@ -0,0 +1,15 @@
+/*
+ *Ler os dados de um trabalhador com N contratos (N fornecido pelo usuário). Depois, solicitar
+ *do usuário um mês e mostrar qual foi o salário do funcionário nesse mês.
+*/
+
+package application;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
