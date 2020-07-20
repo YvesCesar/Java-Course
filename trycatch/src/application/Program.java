dd5be053c32e761150fb5bc191086b597e12a806
Yves César <yvesamorim73@gmail.com>
2020-07-20 16:37:04 -0400

    Stack Trace

--- trycatch/src/application/Program.java
+++ trycatch/src/application/Program.java
@@ -9,6 +9,23 @@ public class Program {
 	public static void main(String[] args) {
 		
 		Locale.setDefault(Locale.US);
+		
+		method1();
+		
+		System.out.println("End of Program");
+		
+	}
+	
+	public static void method1() {
+		System.out.println("***METHOD1 START***");
+		
+		method2();
+		
+		System.out.println("***METHOD1 END***");
+	}
+	
+	public static void method2() {
+		System.out.println("***METHOD2 START***");
 		Scanner sc = new Scanner( System.in );
 		
 		try {
@@ -18,14 +35,16 @@ public class Program {
 		}
 		catch( ArrayIndexOutOfBoundsException e ) {
 			System.out.println("Invalid position!");
+			e.printStackTrace(); //Imprime na tela o rastreamento do Stack
+			//Logo, deve mostrar o caminho de todas as excessões no terminal
+			sc.next();
 		}
 		catch ( InputMismatchException e ) {
 			System.out.println("Input Error");
 		}
 		
-		System.out.println("End of Program");
-		
 		sc.close();
+		System.out.println("***METHOD2 END***");
 	}
-
+	
 }
