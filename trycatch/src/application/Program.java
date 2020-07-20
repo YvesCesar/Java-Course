b66d91a1863cd9bd24b3d8a075d9f65657cb33a7
Yves César <yvesamorim73@gmail.com>
2020-07-20 16:17:23 -0400

    Estrutura try-catch

---
+++ trycatch/src/application/Program.java
@@ -0,0 +1,31 @@
+package application;
+
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner( System.in );
+		
+		try {
+			String[] vect = sc.nextLine().split(" ");
+			int position = sc.nextInt();
+			System.out.println(vect[position]);
+		}
+		catch( ArrayIndexOutOfBoundsException e ) {
+			System.out.println("Invalid position!");
+		}
+		catch ( InputMismatchException e ) {
+			System.out.println("Input Error");
+		}
+		
+		System.out.println("End of Program");
+		
+		sc.close();
+	}
+
+}
