1ca3b15b00155d51ca624977e08da0b0bb9a9d1e
Yves César <yvesamorim73@gmail.com>
2020-08-28 17:14:48 -0400

    Resolvendo problema sem interfaces

---
+++ interfaceProblem/src/model/services/RentalService.java
@@ -0,0 +1,41 @@
+package model.services;
+
+import model.entities.CarRental;
+import model.entities.Invoice;
+
+public class RentalService {
+	
+	private Double pricePerDay;
+	private Double pricePerHour;
+	
+	private BrazilTaxService taxService;
+
+	public RentalService(Double pricePerDay, Double pricePerHour, BrazilTaxService taxService) {
+		this.pricePerDay = pricePerDay;
+		this.pricePerHour = pricePerHour;
+		this.taxService = taxService;
+	}
+	
+	public void processInvoice(CarRental carRental) {
+		long t1 = carRental.getStart().getTime();
+		long t2 = carRental.getFinish().getTime();
+		
+		/*mill -> seg : / 1000
+		 * seg -> min : / 60
+		 * min -> hour : / 60 
+		*/
+		double hours = (double) (t2 - t1) / 1000 / 60 / 60; 
+		double basicPayment;
+		
+		if (hours <= 12.0) {
+			basicPayment = Math.ceil(hours) * pricePerHour;
+		}
+		else {
+			basicPayment = Math.ceil(hours / 24) * pricePerDay;
+		}
+		
+		double tax = taxService.tax(basicPayment);
+		
+		carRental.setInvoice(new Invoice(basicPayment, tax));
+	}
+}
