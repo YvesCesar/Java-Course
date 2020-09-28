8e354ca57d43e2072af102cc39318b584a199b6a
Yves César <yvesamorim73@gmail.com>
2020-09-28 15:20:12 -0400

    Solução Generics(Reuso, Type safety e Performance)

--- GenericsProblem/src/application/Program.java
+++ GenericsProblem/src/application/Program.java
@@ -9,13 +9,13 @@ public class Program {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		PrintService ps = new PrintService();
+		PrintService<String> ps = new PrintService<>();
 		
 		System.out.print("How many values? ");
 		int n = sc.nextInt();
 		
 		for (int i = 0; i < n; i++) {
-			int value = sc.nextInt();
+			String value = sc.next();
 			ps.addValue(value);
 		}
 		
--- GenericsProblem/src/services/PrintService.java
+++ GenericsProblem/src/services/PrintService.java
@@ -3,15 +3,15 @@ package services;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PrintService {
+public class PrintService<T> {
 	
-	private List<Integer> list = new ArrayList<>();
+	private List<T> list = new ArrayList<>();
 	
-	public void addValue(Integer value) {
+	public void addValue(T value) {
 		list.add(value);
 	}
 	
-	public Integer first() {
+	public T first() {
 		if (list.isEmpty()) {
 			throw new IllegalStateException("List is empty");
 		}
