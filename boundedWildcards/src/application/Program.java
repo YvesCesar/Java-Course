37dcc320836bd6a89212e96a19e7750654e10c61
Yves César <yvesamorim73@gmail.com>
2020-10-02 18:05:50 -0400

    Solution

--- boundedWildcards/src/application/Program.java
+++ boundedWildcards/src/application/Program.java
@@ -15,10 +15,14 @@ public class Program {
 		myShapes.add(new Rectangle(3.0, 2.0));
 		myShapes.add(new Circle(2.0));
 		
-		System.out.println("Total area: " + totalArea(myShapes));
+		List<Circle> myCircles = new ArrayList<>();
+		myCircles.add(new Circle(2.0));
+		myCircles.add(new Circle(3.0));
+		
+		System.out.println("Total area: " + totalArea(myCircles));
 	}
 	
-	public static double totalArea(List<Shape> list) {
+	public static double totalArea(List<? extends Shape> list) {
 		double sum = 0.0;
 		for (Shape s : list) {
 			sum += s.area();
