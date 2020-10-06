7275e9f3903f84a6ca33c62e092ee1c8833433a2
Yves César <yvesamorim73@gmail.com>
2020-10-06 17:45:34 -0400

    HashCode and Equals

---
+++ HashCodeAndEquals/src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		String a = "Maria";
+		String b = "Alex";
+		
+		System.out.println(a.equals(b));
+		System.out.println(a.hashCode());
+		System.out.println(b.hashCode());
+	}
+
+}
