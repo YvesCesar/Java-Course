ac34f5e5c14ca27d0190ebceb64a0ebefe0ab68d
Yves César <yvesamorim73@gmail.com>
2020-10-06 18:06:16 -0400

    HashCode and Equals - Entitie Client

--- HashCodeAndEquals/src/application/Program.java
+++ HashCodeAndEquals/src/application/Program.java
@@ -1,15 +1,26 @@
 package application;
 
+import entities.Client;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		String a = "Maria";
-		String b = "Alex";
+		Client c1 = new Client("Maria", "maria@gmail.com");
+		Client c2 = new Client("Bob", "alex@gmail.com");
+		Client c3 = new Client("Maria", "maria@gmail.com");
 		
-		System.out.println(a.equals(b));
-		System.out.println(a.hashCode());
-		System.out.println(b.hashCode());
+		String s1 = "Test";
+		String s2 = "Test";
+		String s3 = new String("Test");
+		String s4 = new String("Test");
+		
+		System.out.println(c1.hashCode());
+		System.out.println(c2.hashCode());
+		System.out.println(c1.equals(c2));
+		System.out.println(c1 == c3); //Compara as referências de memória
+		System.out.println(s1 == s2);
+		System.out.println(s3 == s4);
 	}
 
 }
