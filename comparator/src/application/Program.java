d2838eef37718b778d4a094bce9a621b6d9cc27e
YvesCesar <yvesamorim73@gmail.com>
2020-12-08 17:55:36 -0400

    Third resolution with comparator(and lambda expression)

--- comparator/src/application/Program.java
+++ comparator/src/application/Program.java
@@ -8,22 +8,21 @@ import entities.Product;
 
 public class Program {
 	public static void main(String[] args) {
-		
+
 		List<Product> list = new ArrayList<>();
-		
+
 		list.add(new Product("TV", 900.00));
 		list.add(new Product("Notebook", 1200.00));
 		list.add(new Product("Tablet", 450.00));
-		
-		Comparator<Product> comp = new Comparator<Product>() {
-			@Override
-			public int compare(Product p1, Product p2) {
-				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-			}
+
+		// Definindo uma função anônima com expressões lambda
+		Comparator<Product> comp = (p1, p2) -> {
+			return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
 		};
-		
+		// Sintaxe também conhecida como Arrow Function
+
 		list.sort(comp);
-		
+
 		for (Product p : list) {
 			System.out.println(p);
 		}
