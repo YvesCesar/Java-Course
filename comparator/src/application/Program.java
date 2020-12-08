b6657a7a0e0c90e4b1e641d326a9d3c153f787a3
YvesCesar <yvesamorim73@gmail.com>
2020-12-08 17:32:52 -0400

    First Resolution with Comparator.

--- comparator/src/application/Program.java
+++ comparator/src/application/Program.java
@@ -1,7 +1,6 @@
 package application;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import entities.Product;
@@ -15,7 +14,7 @@ public class Program {
 		list.add(new Product("Notebook", 1200.00));
 		list.add(new Product("Tablet", 450.00));
 		
-		Collections.sort(list);
+		list.sort(new MyComparator());
 		
 		for (Product p : list) {
 			System.out.println(p);
--- comparator/src/entities/Product.java
+++ comparator/src/entities/Product.java
@@ -1,6 +1,6 @@
 package entities;
 
-public class Product implements Comparable<Product>{
+public class Product {
 	
 	private String name;
 	private Double price;
@@ -28,11 +28,6 @@ public class Product implements Comparable<Product>{
 	@Override
 	public String toString() {
 		return "Product [name=" + name + ", price=" + price + "]";
-	}
-	
-	@Override
-	public int compareTo(Product p) {
-		return name.toUpperCase().compareTo(p.getName().toUpperCase());
 	}
 	
 	
