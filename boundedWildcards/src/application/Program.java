4ecf3d91e9bc0672e25d2f947e70e6aee2a193ce
YvesCesar <yvesamorim73@gmail.com>
2020-12-08 16:55:57 -0400

    Initial commit(With Comparator initial problem)

--- boundedWildcards/src/application/Program.java
+++ boundedWildcards/src/application/Program.java
@@ -19,7 +19,7 @@ public class Program {
 		myCircles.add(new Circle(2.0));
 		myCircles.add(new Circle(3.0));
 		
-		System.out.println("Total area: " + totalArea(myCircles));
+		System.out.println("Total area: " + totalArea(myShapes));
 	}
 	
 	public static double totalArea(List<? extends Shape> list) {
