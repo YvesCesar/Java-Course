832da4a418e47b4d77bdfe8a2f29084ca42fabe0
YvesCesar <yvesamorim73@gmail.com>
2020-12-08 18:00:00 -0400

    refactoring code

--- comparator/src/application/Program.java
+++ comparator/src/application/Program.java
@@ -16,10 +16,7 @@ public class Program {
 		list.add(new Product("Tablet", 450.00));
 
 		// Definindo uma função anônima com expressões lambda
-		Comparator<Product> comp = (p1, p2) -> {
-			return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-		};
-		// Sintaxe também conhecida como Arrow Function
+		Comparator<Product> comp = (p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
 
 		list.sort(comp);
 
