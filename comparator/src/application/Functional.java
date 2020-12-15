64177808babf6eaff02e59da76cd06c5a35b5af2
YvesCesar <yvesamorim73@gmail.com>
2020-12-15 08:01:51 -0400

    Method references example

--- comparator/src/application/Functional.java
+++ comparator/src/application/Functional.java
@@ -5,19 +5,22 @@ import java.util.List;
 
 import entities.Product;
 
-public class Program {
+public class Functional {
+	
+	public static int compareProducts(Product p1, Product p2) {
+		return p1.getPrice().compareTo(p2.getPrice());
+	}
+	
 	public static void main(String[] args) {
-
+		
 		List<Product> list = new ArrayList<>();
-
 		list.add(new Product("TV", 900.00));
 		list.add(new Product("Notebook", 1200.00));
 		list.add(new Product("Tablet", 450.00));
-
-		list.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
-
-		for (Product p : list) {
-			System.out.println(p);
-		}
+		
+		list.sort(Functional::compareProducts);
+		
+		list.forEach(System.out::println);
 	}
+
 }
