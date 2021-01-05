1f03d187b0b70b94b87007f708d5f7d3f09d737b
YvesCesar <yvesamorim73@gmail.com>
2021-01-05 19:43:58 -0400

    Product Predicate class example

--- predicate/src/application/Program.java
+++ predicate/src/application/Program.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import entities.Product;
+import util.ProductPredicate;
 
 public class Program {
 
@@ -16,7 +17,7 @@ public class Program {
 		list.add(new Product("Tablet", 350.00));
 		list.add(new Product("HD Case", 80.90));
 		
-		list.removeIf(p -> p.getPrice() >= 100);
+		list.removeIf(new ProductPredicate());
 		
 		for (Product p : list) {
 			System.out.println(p);
