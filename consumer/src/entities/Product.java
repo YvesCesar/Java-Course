ba20ab04624e44140d31b8e2da19dbb0e902f05b
YvesCesar <yvesamorim73@gmail.com>
2021-02-27 18:55:32 -0400

    Consumer - Solution with static method

--- consumer/src/entities/Product.java
+++ consumer/src/entities/Product.java
@@ -22,12 +22,8 @@ public class Product {
 		this.price = price;
 	}
 	
-	public static boolean staticProductPredicate(Product p) {
-		return p.getPrice() >= 100.00;
-	}
-	
-	public boolean nonStaticProductPredicate() {
-		return price >= 100.00;
+	public static void staticPriceUpdate(Product p) {
+		p.setPrice(p.getPrice() * 1.1);
 	}
 	
 	@Override
