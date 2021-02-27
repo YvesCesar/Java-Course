3501593b6c12d2c3943734ba7a6ae82be32f71ff
YvesCesar <yvesamorim73@gmail.com>
2021-02-27 19:04:24 -0400

    Consumer - Solution with inline lambda expression

--- consumer/src/application/Program.java
+++ consumer/src/application/Program.java
@@ -3,7 +3,6 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Consumer;
 
 import entities.Product;
 
@@ -20,11 +19,7 @@ public class Program {
 		
 		double factor = 1.1;
 		
-		Consumer<Product> cons = p -> {
-			p.setPrice(p.getPrice() * factor);
-		};
-		
-		list.forEach(cons);
+		list.forEach(p -> p.setPrice(p.getPrice() * factor));
 		
 		list.forEach(System.out::println);
 	}
