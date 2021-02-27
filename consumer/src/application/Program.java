1d260acf6895835a55d0db2020aabaadafbfc22e
YvesCesar <yvesamorim73@gmail.com>
2021-02-27 19:02:40 -0400

    Consumer - solution with declared lambda expression

--- consumer/src/application/Program.java
+++ consumer/src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Consumer;
 
 import entities.Product;
 
@@ -17,7 +18,13 @@ public class Program {
 		list.add(new Product("Tablet", 350.00));
 		list.add(new Product("HD Case", 80.90));
 		
-		list.forEach(Product::nonStaticPriceUpdate);
+		double factor = 1.1;
+		
+		Consumer<Product> cons = p -> {
+			p.setPrice(p.getPrice() * factor);
+		};
+		
+		list.forEach(cons);
 		
 		list.forEach(System.out::println);
 	}
