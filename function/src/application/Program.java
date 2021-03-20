6037657ebda4cc9b98de2f0096d22d95fbda9e08
YvesCesar <yvesamorim73@gmail.com>
2021-03-19 20:17:15 -0400

    Function - Initial Problem

---
+++ function/src/application/Program.java
@@ -0,0 +1,27 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
+import entities.Product;
+import util.UpperCaseName;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		List<Product> list = new ArrayList<>();
+		
+		list.add(new Product("TV", 900.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Tablet", 350.00));
+		list.add(new Product("HD Case", 80.90));
+		
+		List<String> names = list.stream().map(new UpperCaseName()).collect(Collectors.toList());
+		
+		names.forEach(System.out::println);
+	}
+
+}
