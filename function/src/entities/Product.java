5d4202959673d11318dd9afe5fef0d4fa5fc497a
YvesCesar <yvesamorim73@gmail.com>
2021-03-19 20:34:18 -0400

    Function - Solution with static method

--- function/src/entities/Product.java
+++ function/src/entities/Product.java
@@ -22,12 +22,8 @@ public class Product {
 		this.price = price;
 	}
 	
-	public static void staticPriceUpdate(Product p) {
-		p.setPrice(p.getPrice() * 1.1);
-	}
-	
-	public void nonStaticPriceUpdate() {
-		price = price * 1.1;
+	public static String staticUpperCaseName(Product p) {
+		return p.getName().toUpperCase();
 	}
 	
 	@Override
