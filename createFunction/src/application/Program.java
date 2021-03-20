74e6fc6c27d330fb34ccbcf063a3ae09bb6416fe
YvesCesar <yvesamorim73@gmail.com>
2021-03-19 21:03:50 -0400

    Creating functions that take functions as a parameter - Solution

--- createFunction/src/application/Program.java
+++ createFunction/src/application/Program.java
@@ -15,12 +15,12 @@ public class Program {
 		
 		list.add(new Product("TV", 900.00));
 		list.add(new Product("Mouse", 50.00));
-		list.add(new Product("Tablet", 350.00));
+		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
 		ProductService ps = new ProductService();
 		
-		double sum = ps.filteredSum(list);
+		double sum = ps.filteredSum(list, p -> p.getName().charAt(0) == 'T');
 		
 		System.out.println("Sum = " + String.format("%.2f", sum));
 	}
