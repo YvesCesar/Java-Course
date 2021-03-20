504e4523a7262bf3627bc7a50cebaa0891d547b5
YvesCesar <yvesamorim73@gmail.com>
2021-03-19 20:56:59 -0400

    Creating functions that take functions as a parameter - Initial problem

---
+++ createFunction/src/model/services/ProductService.java
@@ -0,0 +1,18 @@
+package model.services;
+
+import java.util.List;
+
+import entities.Product;
+
+public class ProductService {
+	
+	public double filteredSum(List<Product> list) {
+		double sum = 0.0;
+		for (Product p : list ) {
+			if (p.getName().charAt(0) == 'T') {
+				sum += p.getPrice();
+			}
+		}
+		return sum;
+	}
+}
