6bb55484722279c942e6c1f0bfcd042a08fe81a7
YvesCesar <yvesamorim73@gmail.com>
2021-03-19 20:39:30 -0400

    Function - Solution with declared lambda expression

--- function/src/application/Program.java
+++ function/src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import entities.Product;
@@ -18,7 +19,9 @@ public class Program {
 		list.add(new Product("Tablet", 350.00));
 		list.add(new Product("HD Case", 80.90));
 		
-		List<String> names = list.stream().map(Product::nonStaticUpperCaseName).collect(Collectors.toList());
+		Function<Product, String> func = p -> p.getName().toUpperCase();
+		
+		List<String> names = list.stream().map( func ).collect(Collectors.toList());
 		
 		names.forEach(System.out::println);
 	}
