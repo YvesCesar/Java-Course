000c143d1da56e3aa4f7ee3c475199c5677a6a9a
YvesCesar <yvesamorim73@gmail.com>
2021-03-26 22:02:53 -0400

    Stream - create String stream

--- stream/src/application/Program.java
+++ stream/src/application/Program.java
@@ -13,7 +13,8 @@ public class Program {
 		Stream<Integer> st1 = list.stream().map(x -> x * 10);
 		System.out.println(Arrays.toString(st1.toArray()));
 		
-		
+		Stream<String> st2 = Stream.of("Maria", "Alex", "Bob");
+		System.out.println(Arrays.toString(st2.toArray()));
 	}
 
 }
