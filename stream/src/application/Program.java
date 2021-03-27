a3f9e11ff17c124bdf42214b5be80bd34ac6aade
YvesCesar <yvesamorim73@gmail.com>
2021-03-26 21:54:25 -0400

    Stream - print stream

---
+++ stream/src/application/Program.java
@@ -0,0 +1,19 @@
+package application;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		List<Integer> list = Arrays.asList(3, 4, 5, 10, 7);
+		
+		Stream<Integer> st1 = list.stream();
+		System.out.println(Arrays.toString(st1.toArray()));
+		
+		
+	}
+
+}
